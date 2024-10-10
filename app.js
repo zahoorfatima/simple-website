22db03b1e17343af2d9a74df4feda8e4acfbd2d4
zahoorfatima <zahoorfatima303@gmail.com>
2024-10-10 12:24:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function showSpecial() {
+    var specialElement = document.getElementById('special');
+    specialElement.textContent = "Today's Special: Caramel Macchiato - A perfect blend of caramel, coffee, and milk!";
+    specialElement.style.color = '#ff6f61';
+}
